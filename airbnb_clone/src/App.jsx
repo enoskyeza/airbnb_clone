2bb2ab949ec666c2218d3f523ed7260c7b6b9aaf
enoskyeza <enoskyeza@gmail.com>
2023-11-24 02:19:22 +0300

    Add assets for card section

--- airbnb_clone/src/App.jsx
+++ airbnb_clone/src/App.jsx
@@ -1,39 +1,66 @@
-import { useState } from 'react'
-import Contact from './components/Contacts'
-import './components/Contacts.css'
-import './App.css'
+import React from "react";
+import { useState } from "react";
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import Card from "./components/Card"
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Contact
-      img = 'mr-whiskerson.png'
-      name = 'Mr. Whiskerson'
-      phone = '(212) 555-1234'
-      email = 'mr.whiskaz@catnap.meow'
-      />
-      <Contact
-      img = 'fluffykins.png'
-      name = 'Fluffykins'
-      phone = '(212) 555-2345'
-      email = 'fluff@me.com'
-      />
-      <Contact
-      img = 'felix.png'
-      name = 'Felix'
-      phone = '(212) 555-4567'
-      email = 'thecat@hotmail.com'
-      />
-      <Contact
-      img = 'pumpkin.png'
-      name = 'Pumpkin'
-      phone = '(0800) CAT KING'
-      email = 'pumpkin@scrimba.com'
-      />
+        <Navbar />
+        <Hero />
+        <Card
+            img="katie-zaferes.png"
+            rating="5.0"
+            reviewCount={6}
+            country="USA"
+            title="Life Lessons with Katie Zaferes"
+            price={136}
+        />
+
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
+/*
+Contacts - prop exercise
+
+// import Contact from "./components/Contacts";
+
+function App() {
+  return (
+    <>
+      <div className="contacts">
+        <Contact
+          img="mr-whiskerson.png"
+          name="Mr. Whiskerson"
+          phone="(212) 555-1234"
+          email="mr.whiskaz@catnap.meow"
+        />
+        <Contact
+          img="fluffykins.png"
+          name="Fluffykins"
+          phone="(212) 555-2345"
+          email="fluff@me.com"
+        />
+        <Contact
+          img="felix.png"
+          name="Felix"
+          phone="(212) 555-4567"
+          email="thecat@hotmail.com"
+        />
+        <Contact
+          img="pumpkin.png"
+          name="Pumpkin"
+          phone="(0800) CAT KING"
+          email="pumpkin@scrimba.com"
+        />
+      </div>
+    </>
+  );
+}
+*/
+
--- airbnb_clone/src/components/Contacts.jsx
+++ airbnb_clone/src/components/Contacts.jsx
@@ -1,33 +1,54 @@
 import React from "react"
+import MailIcon from "../assets/mail-icon.png"
+import PhoneIcon from "../assets/phone-icon.png"
+import './Contacts.css'
 
 
 function Contact(props) {
     const {img, name, phone, email} = props
+    console.log(props)
     return (
-        <div className="contacts">
-
             <div className="contact-card">
-                <img src= {`../assets/${img}`} />
+                <img src= {`/images/${img}`} />
                 <h3>{ name }</h3>
                 <div className="info-group">
-                    <img src="../assets/phone-icon.png" />
+                    <img src={PhoneIcon} />
                     <p>{ phone }</p>
                 </div>
                 <div className="info-group">
-                    <img src="../assets/mail-icon.png" />
+                    <img src={MailIcon} />
                     <p>{ email }</p>
                 </div>
             </div>
-
-        </div>
     )
 }
 
 export default Contact
 
 
+/* ARRAY.MAP()
 
-/*
+1. What does the `.map()` array method do?
+Returns a new array. Whatever gets returned from the callback
+function provided is placed at the same index in the new array.
+Usually we take the items from the original array and modify them
+in some way.
+
+
+2. What do we usually use `.map()` for in React?
+Convert an array of raw data into an array of JSX elements
+that can be displayed on the page.
+
+
+3. Why is using `.map()` better than just creating the components
+   manually by typing them out?
+It makes our code more "self-sustaining" - not requiring
+additional changes whenever the data changes.
+
+
+*/
+
+/*  PROPS
 
 1. What do props help us accomplish?
 Make a component more reusable.
